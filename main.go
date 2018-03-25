5cccc66538406f47f07af8551c308ea7e48d996e
Joe Beda <joe.github@bedafamily.com>
2018-03-25 16:17:14 -0700

    I think this works
    Signed-off-by: Joe Beda <joe.github@bedafamily.com>

--- main.go
+++ main.go
@@ -24,20 +24,34 @@ const (
 	boardWidth         = 19.0
 	boardHeight        = 11.0
 	numChunks          = 4
+	unitsPerInch       = 72
+	strokeWidth        = 0.01
 )
-
-// Scales a number between 0 and inMax proportionally to outMin and outMax
-func ScaleNum(in, inMax, outMin, outMax float64) float64 {
-	return outMin + (outMax-outMin)*(in/inMax)
-}
 
 func initColors(n int) []string {
 	r := []string{}
 
 	for i := 0; i < n; i++ {
-		r = append(r, fmt.Sprintf("#00%02x00", int(ScaleNum(float64(i), float64(n), 32, 255))))
+		r = append(r, fmt.Sprintf("#00%02x00", int(scaleToRange(float64(i), float64(n), 32, 255))))
 	}
 	return r
+}
+
+// Scales a number between 0 and inMax proportionally to outMin and outMax
+func scaleToRange(in, inMax, outMin, outMax float64) float64 {
+	return outMin + (outMax-outMin)*(in/inMax)
+}
+
+func scaleValue(f float64) float64 {
+	return f * unitsPerInch
+}
+
+func scaleCoord(c geom.Coord) geom.Coord {
+	return geom.Coord{scaleValue(c.X), scaleValue(c.Y)}
+}
+
+func style(color string) string {
+	return fmt.Sprintf("fill: none; stroke: %s; stroke-width: %g", color, scaleValue(strokeWidth))
 }
 
 func main() {
@@ -48,7 +62,7 @@ func main() {
 
 	xPerChunk := int(xNum / numChunks)
 
-	canvasCenter := geom.Coord{canvasWidth / 2.0, canvasHeight / 2.0}
+	canvasCenter := scaleCoord(geom.Coord{canvasWidth / 2.0, canvasHeight / 2.0})
 	maxDistance := geom.Coord{0, 0}.DistanceFrom(canvasCenter)
 
 	colors := initColors(numChunks)
@@ -56,21 +70,25 @@ func main() {
 	for chunk := 0; chunk < numChunks; chunk++ {
 		// Set up new root for chunk
 		r := svgdata.CreateRoot()
-		r.Attrs()["viewBox"] = fmt.Sprintf("0 0 %g %g", boardWidth, boardHeight)
+		r.Attrs()["viewBox"] = fmt.Sprintf("0 0 %g %g", scaleValue(boardWidth), scaleValue(boardHeight))
 		r.Attrs()["width"] = fmt.Sprintf("%gin", boardWidth)
 		r.Attrs()["height"] = fmt.Sprintf("%gin", boardHeight)
 
 		if chunk == numChunks-1 {
-			outline := svgdata.NewRectXYWH(0, 0, canvasWidth, canvasHeight)
+			outline := svgdata.NewRectXYWH(0, 0, scaleValue(canvasWidth), scaleValue(canvasHeight))
 			r.AddChild(outline)
-			outline.Attrs()["style"] = "fill: none; stroke: red; stroke-width: 0.001"
-			outline.Attrs()["transform"] = fmt.Sprint("translate(%g, %g)", (boardWidth-canvasWidth)/2.0, (boardHeight-canvasHeight)/2.0)
+			outline.Attrs()["style"] = style("red")
+			outline.Attrs()["transform"] = fmt.Sprint("translate(%g, %g)",
+				scaleValue((boardWidth-canvasWidth)/2.0),
+				scaleValue((boardHeight-canvasHeight)/2.0))
 		}
 
 		g := svgdata.NewGroup()
 		r.AddChild(g)
-		g.Attrs()["style"] = fmt.Sprintf("fill: none; stroke: %s; stroke-width: 0.001", colors[chunk])
-		g.Attrs()["transform"] = fmt.Sprintf("translate(%g, %g)", (boardWidth-canvasWidth)/2.0, (boardHeight-canvasHeight)/2.0)
+		g.Attrs()["style"] = style(colors[chunk])
+		g.Attrs()["transform"] = fmt.Sprintf("translate(%g, %g)",
+			scaleValue((boardWidth-canvasWidth)/2.0),
+			scaleValue((boardHeight-canvasHeight)/2.0))
 
 		chunkStart := chunk * xPerChunk
 		chunkEnd := (chunk + 1) * xPerChunk
@@ -81,17 +99,17 @@ func main() {
 		for x := chunkStart; x < chunkEnd; x++ {
 
 			for y := 0; y < yNum; y++ {
-				c := geom.Coord{
+				c := scaleCoord(geom.Coord{
 					canvasMargin + cSpace/2 + float64(x)*xSpace,
 					canvasMargin + cSpace/2 + float64(y)*ySpace,
-				}
+				})
 				rRaw := c.DistanceFrom(canvasCenter)
-				rad := ScaleNum(rRaw, maxDistance, cMinRadius, cMaxRadius)
+				rad := scaleValue(scaleToRange(rRaw, maxDistance, cMinRadius, cMaxRadius))
 				g.AddChild(svgdata.NewCircle(c, rad))
 			}
 		}
 
 		d, _ := svgdata.Marshal(r, true)
-		ioutil.WriteFile(fmt.Sprintf("output%03d.svg", chunk), d, 0644)
+		ioutil.WriteFile(fmt.Sprintf("test-%03d.svg", chunk), d, 0644)
 	}
 }
